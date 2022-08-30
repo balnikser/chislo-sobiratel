0029c84bca4f04b9fd1de7312963e6bf040e8a93
balnikser <balnikser@yandex.ru>
2022-08-30 11:46:53 +0300

    field-generation-score

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
+function roll(lowest, highest){
+    return Math.ceil(lowest + Math.random() * (highest - lowest));
+}
+const x5 = "x 5";
+const d5 = "÷ 5";
+const empty = " 0 ";
+const unknown = "???"
 class Field{
-    data = {};
     get(position){
-        return "" + Math.abs(position[0])%10 + ';' + Math.abs(position[1])%10
-        if(this.data[position] === undefined){
-            this.data[position] = "???";
-        }
-        return this.data[position]
+        if(this[position] === undefined){
+            this[position] = unknown;
+        } else if(this[position] === unknown){
+            this[position] = ' ? ';
+        } else if (this[position] === ' ? '){
+            let generated;
+            const generate_roll = roll(-99, 99);
+            if (0 < generate_roll && generate_roll < 10){
+                generated = x5
+            } else if (-10 < generate_roll && generate_roll < 0){
+                generated = d5
+            } else if (generate_roll === 0){
+                generated = empty;
+            } else {
+                if(generate_roll > 0){
+                    generated = "+" + generate_roll;
+                } else {
+                    generated = generate_roll;
+                }
+            }
+            this[position] = generated;
+        }/* else if (Number(this[position]) < 0){
+            this[position] = " " + (Number(this[position]) + 1);
+            this[position] = this[position].substring(this[position].length - 3);
+        }*/
+        return this[position]
     }
 };
 class App{
@@ -32,13 +59,31 @@ class App{
     }
     log(text){
         var entry = document.createElement('li');
-        entry.appendChild(document.createTextNode(text + ";score: " + this.score));
+        entry.appendChild(document.createTextNode(text + " = " + this.score));
         this.logElement.appendChild(entry);
     }
+    move(vector){
+        this.field[this.position] = " -5";
+        this.position = this.fromPosition(vector);
+        if(this.field[this.position] === ' ? '){
+            this.field.get(this.position);
+        }
+        //this.log("step on " + this.position + this.field[this.position]);
+        if (this.field[this.position] === x5){
+            this.score *= 5;
+            this.log("x5");
+        } else if (this.field[this.position] === d5){
+            this.score = Math.floor(this.score/5);
+            this.log("/5");
+        } else if (Number(this.field[this.position])){
+            this.score += Number(this.field[this.position]);
+            this.log("add score: " + this.field[this.position]);
+        }
+
+    }
     buttonPressed(id){
-        this.score += 1;
         console.log(this.vectorAtDirection[id]);
-        this.position = this.fromPosition(this.vectorAtDirection[id]);
+        this.move(this.vectorAtDirection[id]);
         this.draw();
     }
     draw(){
@@ -61,7 +106,8 @@ class App{
                    + '│'
                    + this.field.get(this.fromPosition([-1, 0]))
                    + '│'
-                   + this.field.get(this.fromPosition([0, 0]))
+                   //+ this.field.get(this.fromPosition([0, 0]))
+                   + ' Ⰰ '
                    + '│'
                    + this.field.get(this.fromPosition([1, 0]))
                    + '│'
